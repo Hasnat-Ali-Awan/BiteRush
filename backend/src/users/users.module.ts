9b82d3e01e4b13254e97ee860cee0827dc8b9152
unknown <hasnataliawan088@gmail.com>
2026-08-06 12:33:35 +0500

    fix: add missing newlines at the end of users.controller.ts, users.module.ts, and users.service.ts

--- backend/src/users/users.module.ts
+++ backend/src/users/users.module.ts
@@ -9,7 +9,8 @@ import { User, UserSchema } from './schemas/user.schema';
   imports: [
     MongooseModule.forFeature([
       { name: User.name, schema: UserSchema },
-    ]),
+    ]
+  ),
   ],
   controllers: [UsersController],
   providers: [UsersService],
